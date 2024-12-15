f639528afe451c17c698495a84fd86dcc992ed60
niopking <120129384+niopking@users.noreply.github.com>
2024-12-15 21:15:47 +0100

    Update app.py Boris
    promjena 1.0

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from g4f.client import Client
+from g4f.client import Client as OpenAI
 import platform
 import asyncio
 
@@ -10,22 +10,19 @@ app = Flask(__name__)
 def home():
     return "Welcome to the Flask app!"
 
-# Route for the tourist attractions
+zn# Route for the tourist attractions
 @app.route('/t')
-async def t():
-    if platform.system() == 'Windows':
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
-    client = Client()
+def t():
+    client = OpenAI()
     prompt = "Give me 10 tourist attractions within a radius of 5 km from the coordinates 44°46'11.6 N 17°09'15.5 E. Print in attraction format: distance: description"
     
-    # Making the asynchronous call
-    response = await client.chat.completions.create(
+    response = client.chat.completions.create(
         model="gpt-4o",
         messages=[{"role": "user", "content": prompt}]
     )
     
     return response.choices[0].message.content
+
 
 
 if __name__ == '__main__':
