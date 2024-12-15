d43a6677bd0fead80cb4b9cd6b8b2840167d2aef
niopking <120129384+niopking@users.noreply.github.com>
2024-12-15 19:46:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from g4f.client import Client
+import platform
+import asyncio
 
 app = Flask(__name__)
 
 @app.route('/')  # This handles GET requests to "/"
 def home():
-    return "Welcome to my Flask app!"
+    if platform.system()=='Windows':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    client = Client()
+    prompt = "Give me 10 tourist attractions within a radius of 5 km from the coordinates 44°46'11.6 N 17°09'15.5 E. Print in attraction format: distance: description"
+    response = client.chat.completions.create(
+                    model="gpt-4o",
+                    messages=[{"role": "user", "content": prompt}]
+                )
+    return response.choices[0].message.content
 
 if __name__ == '__main__':
     app.run()
